58414bc0ee0885127d62fd7a2bdaa9b13fdd8c10
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-07 16:29:01 -0400

    Test Computation

--- bill_splitter.js
+++ bill_splitter.js
@@ -23,4 +23,8 @@ let bills = [275, 40, 430, 125, 555, 44]; //define dataset 1 and 2
 let tips = bills.map(calculateTip); // calculate the tip based off the bill from the array
 let totals = bills.map((bills, index) => bills+tips[index]); // Sums tips and bills for total amount.
 
-//
+// Task 5 test data
+
+console.log("Bills:" bills); // output 275 40 430 125 555 44
+console.log("Tips:", tips); // output 41.25 8 18.75 111 8.8
+console.log("Totals:", totals); //output 316.25 48 516 143.75 666 52.8
