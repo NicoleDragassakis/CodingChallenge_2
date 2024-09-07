268a5209d760c972827d7bda5ac7922580ce5b3c
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-07 14:53:55 -0400

    Add output functionality to display bill,tip,and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -3,3 +3,7 @@
 let bill = 87.00; //bill amount
 let tip = (bill<=300) ? 0.15*bill : 0.20*bill; //when the bill is less than 300, 15% gratituity applies if the bill is greater than 300 20% applies
 
+//Task 2 Output Details
+console.log("Bill:", bill); // output Bill: 87.00
+console.log("Tip:", tip);  // output Tip: 13.05
+console.log("Total Amount:", bill+tip); // output Total amount: 100.05
