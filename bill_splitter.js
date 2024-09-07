a03983708ae0381447f60cfa4f4721c8e9408241
Nicole Dragassakis <niki_dragasakis@icloud.com>
2024-09-07 14:42:20 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,5 @@
+//Task 1 Tip Calculation
+
+let bill = 87.00; //bill amount
+let tip = (bill<=300) ? 0.15*bill : 0.20*bill; //when the bill is less than 300, 15% gratituity applies if the bill is greater than 300 20% applies
+
